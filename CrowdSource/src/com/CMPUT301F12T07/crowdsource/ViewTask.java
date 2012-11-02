a1a440ecccfce599ea643fc518dd160c527c9bee
ba6 <ba6@ualberta.ca>
2012-11-02 12:28:02 -0600

    Copied over a bunch of code from https://github.com/ekoedmedia/BatteryTrack/blob/master/src/com/ekoedmedia/batterytrack/EditTask.java Working on converting into something useable

--- CrowdSource/src/com/CMPUT301F12T07/crowdsource/ViewTask.java
+++ CrowdSource/src/com/CMPUT301F12T07/crowdsource/ViewTask.java
@@ -4,15 +4,86 @@ import android.R;
 import android.app.Activity;
 import android.os.Bundle;
 import android.view.Menu;
+import android.view.View;
+import android.view.View.OnFocusChangeListener;
+import android.widget.TextView;
 
 public class ViewTask extends Activity {
+	
+	
+	private Task currentTask;
+	//private DatabaseHandler db;
+
+	private TextView taskTitle;
+	private TextView startDate;
+	private TextView endDate;
+	private TextView taskDesc;
+	private TextView taskContent;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_view_task);
         
-        Task curentObject = getIntent().getExtras().getParcelable(@string/TaskObject);
+        Task curentTask = getIntent().getExtras().getParcelable(@string/TaskObject);
+        
+        // Getting the start Date field
+        this.startDate = (TextView) findViewById(R.id.textViewCreatedDate);
+
+        // unknown implimentation of task date getter
+        int startDateValue = this.currentTask.getStart();
+        startDate.setText(Integer.toString(startDateValue));
+        
+        /*
+        timeEdit.setOnFocusChangeListener(new OnFocusChangeListener() {
+        	public void onFocusChange(View v, boolean hasFocus){
+        		if (!hasFocus){
+        			currentTask.setTime(Integer.parseInt(timeEdit.getText().toString()));
+        			db.updateTask(currentTask);
+        		}
+        	}
+        }); */
+
+        /*
+        // Getting and Setting of Start Battery field
+        this.startEdit = (TextView) findViewById(R.id.taskStartBattery);
+        double startValue = this.currentTask.getStart();
+        startEdit.setText(Double.toString(startValue));
+        startEdit.setOnFocusChangeListener(new OnFocusChangeListener() {
+        	public void onFocusChange(View v, boolean hasFocus){
+        		if (!hasFocus){
+        			currentTask.setStart(Double.parseDouble(startEdit.getText().toString()));
+        			db.updateTask(currentTask);
+        		}
+        	}
+        });
+
+        // Getting and Setting of End Battery field
+        this.endEdit = (TextView) findViewById(R.id.taskEndBattery);
+        double endValue = this.currentTask.getEnd();
+        endEdit.setText(Double.toString(endValue));
+        endEdit.setOnFocusChangeListener(new OnFocusChangeListener() {
+        	public void onFocusChange(View v, boolean hasFocus){
+        		if (!hasFocus){
+        			currentTask.setEnd(Double.parseDouble(endEdit.getText().toString()));
+        			db.updateTask(currentTask);
+        		}
+        	}
+        });
+
+        // Getting and Setting of Description field
+        this.descEdit = (TextView) findViewById(R.id.taskDesc);
+        String descValue = this.currentTask.getDesc();
+        descEdit.setText(descValue);
+        descEdit.setOnFocusChangeListener(new OnFocusChangeListener() {
+        	public void onFocusChange(View v, boolean hasFocus){
+        		if (!hasFocus){
+        			currentTask.setDesc(descEdit.getText().toString());
+        			db.updateTask(currentTask);
+        		}
+        	}
+        });
+        */
     }
 
     @Override
