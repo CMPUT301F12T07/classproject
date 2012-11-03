e687d1d468e4a64f3ddf11018ccdb3471561087e
Thomas Fung <thomas.fung1@gmail.com>
2012-11-03 14:44:28 -0600

    fix sync issue

--- CrowdSource/src/com/CMPUT301F12T07/crowdsource/ViewTask.java
+++ CrowdSource/src/com/CMPUT301F12T07/crowdsource/ViewTask.java
@@ -1,6 +1,5 @@
 package com.CMPUT301F12T07.crowdsource;
 
-import android.R;
 import android.R.string;
 import android.app.Activity;
 import android.os.Bundle;
@@ -8,8 +7,8 @@ import android.view.Menu;
 import android.widget.TextView;
 
 public class ViewTask extends Activity {
-	
-	
+
+
 	private Task currentTask;
 	//private DatabaseHandler db;
 
@@ -18,45 +17,45 @@ public class ViewTask extends Activity {
 	private TextView endDate;
 	private TextView taskContent;
 	private TextView taskDesc;
-	
+
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_view_task);
         
-        Task curentTask = getIntent().getExtras().getParcelable(@string/TaskObject);
+        this.currentTask = getIntent().getExtras().getParcelable("taskObject");
         
         // Getting the task title field
         this.taskTitle = (TextView) findViewById(R.id.textViewTitle);
         // unknown implimentation of task getters
-        string titleStr = this.currentTask.get_title();
-        taskDesc.setText(titleStr));
+        String titleStr = currentTask.get_title();
+        taskTitle.setText(titleStr);
         
         // Getting the start Date field
         this.startDate = (TextView) findViewById(R.id.textViewCreatedDate);
         // unknown implimentation of task getters
-        int startDateValue = this.currentTask.get_dateCreate();
+        int startDateValue = Integer.parseInt(currentTask.get_dateCreate());
         startDate.setText(Integer.toString(startDateValue));
         
         // Getting the end Date field
         this.endDate = (TextView) findViewById(R.id.textViewDueDate);
         // unknown implimentation of task getters
-        int endDateValue = this.currentTask.get_dateDue();
+        int endDateValue = Integer.parseInt(currentTask.get_dateDue());
         endDate.setText(Integer.toString(endDateValue));
         
         // Getting the task content field
         // type? content?
         this.taskContent = (TextView) findViewById(R.id.textViewContent);
         // unknown implimentation of task getters
-        string contentStr = this.currentTask.get_type();
-        taskDesc.setText(contentStr));
+        String contentStr = currentTask.get_type();
+        taskDesc.setText(contentStr);
         
         // Getting the task description field
         this.taskDesc = (TextView) findViewById(R.id.textViewDescription);
         // unknown implimentation of task getters
-        string descStr = this.currentTask.get_description();
-        taskDesc.setText(descStr));
+        String descStr = currentTask.get_description();
+        taskDesc.setText(descStr);
         
         /*
         timeEdit.setOnFocusChangeListener(new OnFocusChangeListener() {
