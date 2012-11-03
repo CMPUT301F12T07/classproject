ea27af9bede55720fbe50c49f7e316b35081769e
Jeremy Smereka <ekeodmedia@gmail.com>
2012-11-03 14:23:07 -0600

    Fixed CrowdSource Main

--- CrowdSource/src/com/CMPUT301F12T07/crowdsource/MainActivity.java
+++ CrowdSource/src/com/CMPUT301F12T07/crowdsource/MainActivity.java
@@ -1,12 +1,9 @@
 package com.CMPUT301F12T07.crowdsource;
 
-import android.R;
-import android.app.Activity;
 import java.util.List;
 
 import android.app.Activity;
 import android.content.Intent;
-
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.View;
@@ -40,16 +37,9 @@ public class MainActivity extends Activity {
         });
     }
 
-    
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.activity_main, menu);
         return true;
     }
-    
-    /* Function to call ViewTask
-     * Intent intent = new intent(this, ViewTask.class);
-     * intent.putExtra(@string/TaskObject, database.getTaskById(Id));  // database call needs to be fixed
-     * startActivity(intent);
-     */
 }
--- CrowdSource/src/com/CMPUT301F12T07/crowdsource/TaskListAdapter.java
+++ CrowdSource/src/com/CMPUT301F12T07/crowdsource/TaskListAdapter.java
@@ -19,26 +19,25 @@ public class TaskListAdapter extends BaseAdapter {
 		mLayoutInflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
 	}
 	
-	@Override
-    public int getCount() {
+	public int getCount() {
         //getCount() represents how many items are in the list
         return mListItems.size();
     }
  
-    @Override
+    
     //get the data of an item from a specific position
     //i represents the position of the item in the list
     public Object getItem(int i) {
         return null;
     }
  
-    @Override
+    
     //get the position id of the item from the list
     public long getItemId(int i) {
         return 0;
     }
  
-    @Override
+    
     public View getView(int position, View view, ViewGroup viewGroup) {
  
         //check to see if the reused view is null or not, if is not null then reuse it
