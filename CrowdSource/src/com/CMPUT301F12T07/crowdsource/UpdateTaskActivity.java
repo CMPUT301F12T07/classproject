756d2e53ad2313cd9bd0d5525725b85684e512d2
ba6 <ba6@ualberta.ca>
2012-11-04 15:57:20 -0700

    update to Update Task

--- CrowdSource/src/com/CMPUT301F12T07/crowdsource/UpdateTaskActivity.java
+++ CrowdSource/src/com/CMPUT301F12T07/crowdsource/UpdateTaskActivity.java
@@ -1,9 +1,12 @@
 package com.CMPUT301F12T07.crowdsource;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.Menu;
+import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 
 public class UpdateTaskActivity extends Activity {
@@ -14,6 +17,7 @@ public class UpdateTaskActivity extends Activity {
 	private EditText endDate;
 	private EditText taskContent;
 	private EditText taskDesc;
+	private LocalDB db;
 	
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -21,10 +25,16 @@ public class UpdateTaskActivity extends Activity {
         setContentView(R.layout.activity_update_task);
 //        getActionBar().setDisplayHomeAsUpEnabled(true);
 
+<<<<<<< HEAD
+        db = new LocalDB(this);
+        this.currentTask = db.getTask(getIntent().getExtras().getInt("taskObject"));
+
+=======
         LocalDB db = new LocalDB(this);
         this.currentTask = db.getTask(getIntent().getExtras().getInt("taskID"));
         db.close();
         
+>>>>>>> branch 'master' of https://github.com/CMPUT301F12T07/classproject.git
         // Getting the task title field
         this.taskTitle = (EditText) findViewById(R.id.textEditTitle);
         //String titleStr = currentTask.get_title();
@@ -48,6 +58,27 @@ public class UpdateTaskActivity extends Activity {
         this.taskDesc = (EditText) findViewById(R.id.textEditDescription);
         //String descStr = currentTask.get_description();
         taskDesc.setText(currentTask.get_description());
+        
+        final Button Cancel = (Button) findViewById(R.id.buttonCancel);
+        Cancel.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+            	finish();
+            }
+        });
+        
+        Button Save = (Button) findViewById(R.id.buttonSave);
+        Save.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+            	// a lot of work
+            	currentTask.set_title(taskTitle.getText().toString());
+            	currentTask.set_dateCreate(startDate.getText().toString());
+            	currentTask.set_dateDue(endDate.getText().toString());
+            	currentTask.set_type(taskContent.getText().toString());
+            	currentTask.set_description(taskDesc.getText().toString());
+            	
+            	db.updateTask(currentTask);
+            }
+        });
     }
 
     @Override
