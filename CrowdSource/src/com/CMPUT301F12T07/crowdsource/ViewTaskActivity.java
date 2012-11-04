9524d8a699f96500550e206e426a525816c863e9
ba6 <ba6@ualberta.ca>
2012-11-04 14:39:15 -0700

    Linked update button to launching updateTaskActivity

--- CrowdSource/src/com/CMPUT301F12T07/crowdsource/ViewTaskActivity.java
+++ CrowdSource/src/com/CMPUT301F12T07/crowdsource/ViewTaskActivity.java
@@ -1,8 +1,11 @@
 package com.CMPUT301F12T07.crowdsource;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 public class ViewTaskActivity extends Activity {
@@ -59,6 +62,15 @@ public class ViewTaskActivity extends Activity {
         		}
         	}
         }); */
+        
+        final Button Update = (Button) findViewById(R.id.buttonUpdate);
+        Update.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+            	Intent intent = new Intent(ViewTaskActivity.this, UpdateTaskActivity.class);
+        		intent.putExtra("taskObject", currentTask);
+        		startActivity(intent);
+            }
+        });
     }
 
     @Override
