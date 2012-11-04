42ff189c9d9c0cdf5f4eb9b737f4ad0f04b4e542
Thomas Fung <thomas.fung1@gmail.com>
2012-11-04 12:23:31 -0700

    add task is set up except for save

--- CrowdSource/src/com/CMPUT301F12T07/crowdsource/AddTaskActivity.java
+++ CrowdSource/src/com/CMPUT301F12T07/crowdsource/AddTaskActivity.java
@@ -5,21 +5,43 @@ import java.util.Calendar;
 import android.os.Bundle;
 import android.app.Activity;
 import android.app.DatePickerDialog;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.Menu;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
+import android.widget.CompoundButton.OnCheckedChangeListener;
 import android.widget.DatePicker;
+import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
 public class AddTaskActivity extends Activity {
 
-	private Button calendar;
-	private TextView textDate;
+	private Button selectDate;
+	private TextView selectedDate;
 	private int year;
 	private int month;
 	private int day;
+	
+	private EditText titleText;
+	private EditText descriptionText;
+	private EditText quantityText;
+	private String title;
+	private String description;
+	private int quantity;
+	
+	private CheckBox privacyCheckBox;
+	private int visibility;
+	
+	private Button save;
+	
+//	private Button selectQuantity;
+//	private TextView selectedQuantity;
+//	private int quantity;
 	
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -35,24 +57,26 @@ public class AddTaskActivity extends Activity {
         return true;
     }
     
-    private void initializeDates() {
+    private void initializeListeners() {
+    	initializeDates();
+    	initializeTextFields();
+    	initializeCheckBox();
+    	initializeSave();
+//    	initializeQuantity();
+    }
+
+	private void initializeDates() {
+    	selectDate = (Button) findViewById(R.id.selectDateButton);
+    	selectedDate = (TextView) findViewById(R.id.dateTextView);
     	final Calendar cal = Calendar.getInstance();
     	
     	year = cal.get(Calendar.YEAR);
     	month = cal.get(Calendar.MONTH);
     	day = cal.get(Calendar.DAY_OF_MONTH);
     	
-    }
-    
-    private void initializeListeners() {
-    	calendar = (Button) findViewById(R.id.selectDateButton);
-    	textDate = (TextView) findViewById(R.id.dateTextView);
+    	selectedDate.setText((month+1) + "/" + day + "/" + year);
     	
-    	initializeDates();
-    	
-    	textDate.setText((month+1) + "/" + day + "/" + year);
-    	
-    	calendar.setOnClickListener(new OnClickListener() {
+    	selectDate.setOnClickListener(new OnClickListener() {
     		
 			@Override
 			public void onClick(View v) {
@@ -63,18 +87,122 @@ public class AddTaskActivity extends Activity {
     	});
     }
     
-    
     private DatePickerDialog.OnDateSetListener mDateSetListener = new DatePickerDialog.OnDateSetListener() {
 	    public void onDateSet(DatePicker v, int inyear, int inmonth, int inday) {
 	    	year = inyear;
 	        month = inmonth;
 	        day = inday;
 	        
-	        textDate.setText((month+1) + "/" + day + "/" + year);
+	        selectedDate.setText((month+1) + "/" + day + "/" + year);
 	        
 	        Toast.makeText(v.getContext(), "Date set to: " + (month+1) + "/" + day + "/" + year, Toast.LENGTH_SHORT).show();
 	    }
     };
     
+    
+    private void initializeTextFields() {
+    	titleText = (EditText) findViewById(R.id.titleText);
+    	descriptionText = (EditText) findViewById(R.id.descriptionText);
+    	quantityText = (EditText) findViewById(R.id.quantityText);
+    	
+    	title = "";
+    	description = "";
+    	quantity = 0;
+		
+    	titleText.addTextChangedListener(new TextWatcher() {
 
+			@Override
+			public void afterTextChanged(Editable s) {
+				title = s.toString();
+			}
+
+			@Override
+			public void beforeTextChanged(CharSequence arg0, int arg1,
+					int arg2, int arg3) {}
+
+			@Override
+			public void onTextChanged(CharSequence arg0, int arg1, int arg2,
+					int arg3) {}
+    	});
+    	
+    	descriptionText.addTextChangedListener(new TextWatcher() {
+
+			@Override
+			public void afterTextChanged(Editable s) {
+				description = s.toString();
+			}
+
+			@Override
+			public void beforeTextChanged(CharSequence arg0, int arg1,
+					int arg2, int arg3) {}
+
+			@Override
+			public void onTextChanged(CharSequence arg0, int arg1, int arg2,
+					int arg3) {}
+    	});
+    	
+    	quantityText.addTextChangedListener(new TextWatcher() {
+
+			@Override
+			public void afterTextChanged(Editable s) {
+				quantity = Integer.parseInt(s.toString());
+			}
+
+			@Override
+			public void beforeTextChanged(CharSequence arg0, int arg1,
+					int arg2, int arg3) {}
+
+			@Override
+			public void onTextChanged(CharSequence arg0, int arg1, int arg2,
+					int arg3) {}
+    	});
+	}
+    
+    
+    private void initializeCheckBox() {
+    	privacyCheckBox = (CheckBox) findViewById(R.id.privacyCheckbox);
+    	
+    	privacyCheckBox.setOnCheckedChangeListener(new OnCheckedChangeListener() {
+
+			@Override
+			public void onCheckedChanged(CompoundButton button, boolean checked) {
+				if (checked) visibility = 1;
+				else visibility = 0;
+			}
+    		
+    	});
+    }
+    
+    private void initializeSave() {
+    	save = (Button) findViewById(R.id.saveButton);
+    	
+    	save.setOnClickListener(new OnClickListener() {
+
+			@Override
+			public void onClick(View v) {
+				
+				
+			}
+    		
+    	});
+    }
+//    private void initializeQuantity() {
+//    	quantity = 0;
+//    	
+//
+//    	selectQuantity = (Button) findViewById(R.id.selectQuantityButton);
+//    	selectedQuantity = (TextView) findViewById(R.id.quantityTextView);
+//    	
+//   	selectedQuantity.setText(0);
+//    	
+//    	selectQuantity.setOnClickListener(new OnClickListener() {
+//
+//			@Override
+//			public void onClick(View arg0) {
+//				NumberPicker
+//				
+//			}
+//    		
+//    	});
+//    }
 }
