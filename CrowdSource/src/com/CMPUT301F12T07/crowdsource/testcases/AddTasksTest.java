bc20aeff3beaae7d120efbeac535fb8734d29918
tfung <tfung@ualberta.ca>
2012-11-11 13:43:55 -0700

    new test package

---
+++ CrowdSource/src/com/CMPUT301F12T07/crowdsource/testcases/AddTasksTest.java
@@ -0,0 +1,13 @@
+package com.CMPUT301F12T07.crowdsource.testcases;
+
+import junit.framework.TestCase;
+
+public class AddTasksTest extends TestCase {
+	private AddTasksTest addTasksTest;
+	
+	public void testAdding() {
+		
+	}
+	
+	
+}
