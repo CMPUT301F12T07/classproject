0e34135ae924e39334e3bd41b07e3fadab7204d6
ba6 <ba6@ualberta.ca>
2012-11-18 15:49:53 -0700

    New files for logged activity viewing but no implimentation yet

---
+++ CrowdSource/src/com/CMPUT301F12T07/crowdsource/ViewLoggedActivity.java
@@ -0,0 +1,20 @@
+package com.CMPUT301F12T07.crowdsource;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.Menu;
+
+public class ViewLoggedActivity extends Activity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_view_logged);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.activity_view_logged, menu);
+        return true;
+    }
+}
