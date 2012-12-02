63349314aebd54dd587b0574f8c35395ca315a93
ekoedmedia <ekoedmedia@gmail.com>
2012-12-01 17:58:57 -0700

    - Added Date Selector to UpdateTask as well as finished altering its layout. If time available, Task view should be modified to look similar.

--- CrowdSource/src/com/CMPUT301F12T07/crowdsource/UpdateTaskActivity.java
+++ CrowdSource/src/com/CMPUT301F12T07/crowdsource/UpdateTaskActivity.java
@@ -1,5 +1,6 @@
 package com.CMPUT301F12T07.crowdsource;
 
+import java.text.SimpleDateFormat;
 import java.util.Calendar;
 
 import android.app.Activity;
@@ -23,96 +24,191 @@ public class UpdateTaskActivity extends Activity {
 	private Task currentTask;
 	private EditText taskTitle;
 	private TextView startDate;
-	private EditText endDate;
+	private TextView endDate;
 	private EditText taskQuantity;
 	private EditText taskDesc;
 	private LocalDB db;
-	
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_update_task);
-//        getActionBar().setDisplayHomeAsUpEnabled(true);
 
-        db = new LocalDB(this);
-        this.currentTask = db.getTask(getIntent().getExtras().getLong("taskID"));
-        db.close();
-        
-        // Getting the task title field
-        this.taskTitle = (EditText) findViewById(R.id.textEditTitle);
-        taskTitle.setText(currentTask.get_title());
+	private Button selectDate;
 
-        // Getting the start Date field
-        this.startDate = (TextView) findViewById(R.id.textEditCreatedDate);
-        startDate.setText(currentTask.get_dateCreate());
+	private int cYear;
+	private int cMonth;
+	private int cDay;
 
-        // Getting the end Date field
-        this.endDate = (EditText) findViewById(R.id.textEditDueDate);
-        endDate.setText(currentTask.get_dateDue());
+	private int year;
+	private int month;
+	private int day;
 
-        // Getting the task quantity field
-        this.taskQuantity = (EditText) findViewById(R.id.textEditQuantity);
-        taskQuantity.setText(Integer.toString(currentTask.get_quantity()));
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_update_task);
+		// getActionBar().setDisplayHomeAsUpEnabled(true);
 
-        // Getting the task description field
-        this.taskDesc = (EditText) findViewById(R.id.textEditDescription);
+		db = new LocalDB(this);
+		this.currentTask = db
+				.getTask(getIntent().getExtras().getLong("taskID"));
+		db.close();
 
-        taskDesc.setText(currentTask.get_description());
-        
-        final Button Cancel = (Button) findViewById(R.id.buttonCancel);
-        Cancel.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v) {
-            	finish();
-            }
-        });
-        
-        final CheckBox Public = (CheckBox) findViewById(R.id.checkboxPublic);
-        if (currentTask.get_visibility() == 0)
-        	Public.setChecked(true);
-        
-        Public.setOnClickListener(new View.OnClickListener()
-        {
-        	public void onClick(View v)
-        	{
-        		if (Public.isChecked())
-        			currentTask.set_visibility(0);
-        		else
-        			currentTask.set_visibility(1);
-        	}
-        });
- 
-        
-        Button Save = (Button) findViewById(R.id.buttonSave);
-        Save.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v) {
-            	currentTask.set_title(taskTitle.getText().toString());
-            	currentTask.set_dateCreate(startDate.getText().toString());
-            	currentTask.set_dateDue(endDate.getText().toString());
-            	currentTask.set_quantity(Integer.parseInt(taskQuantity.getText().toString()));
-            	currentTask.set_description(taskDesc.getText().toString());
-            	
-            	db.updateTask(currentTask, "");
-            	
-            	finish();
-            }
-        });
-    }
-    
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.activity_update_task, menu);
-        return true;
-    }
+		// Getting the task title field
+		this.taskTitle = (EditText) findViewById(R.id.textEditTitle);
+		taskTitle.setText(currentTask.get_title());
 
-    
-//    @Override
-//    public boolean onOptionsItemSelected(MenuItem item) {
-//        switch (item.getItemId()) {
-//            case android.R.id.home:
-//                NavUtils.navigateUpFromSameTask(this);
-//                return true;
-//        }
-//        return super.onOptionsItemSelected(item);
-//    }
+		// Getting the start Date field
+		this.startDate = (TextView) findViewById(R.id.textEditCreatedDate);
+		startDate.setText(currentTask.get_dateCreate());
+
+		// Getting the end Date field
+		this.endDate = (TextView) findViewById(R.id.textEditDueDate);
+		endDate.setText(currentTask.get_dateDue());
+
+		// Getting the task quantity field
+		this.taskQuantity = (EditText) findViewById(R.id.textEditQuantity);
+		taskQuantity.setText(Integer.toString(currentTask.get_quantity()));
+
+		// Getting the task description field
+		this.taskDesc = (EditText) findViewById(R.id.textEditDescription);
+
+		taskDesc.setText(currentTask.get_description());
+
+		final Button Cancel = (Button) findViewById(R.id.buttonCancel);
+		Cancel.setOnClickListener(new View.OnClickListener() {
+			public void onClick(View v) {
+				finish();
+			}
+		});
+
+		final CheckBox Public = (CheckBox) findViewById(R.id.checkboxPublic);
+		if (currentTask.get_visibility() == 0)
+			Public.setChecked(true);
+
+		Public.setOnClickListener(new View.OnClickListener() {
+			public void onClick(View v) {
+				if (Public.isChecked())
+					currentTask.set_visibility(0);
+				else
+					currentTask.set_visibility(1);
+			}
+		});
+
+		Button Save = (Button) findViewById(R.id.buttonSave);
+		Save.setOnClickListener(new View.OnClickListener() {
+			public void onClick(View v) {
+				if (checkDate() == false) {
+					Toast.makeText(v.getContext(),
+							"Minimum date is " + startDate.getText().toString(), Toast.LENGTH_SHORT)
+							.show();
+				} else {
+					currentTask.set_title(taskTitle.getText().toString());
+					currentTask.set_dateCreate(startDate.getText().toString());
+					currentTask.set_dateDue(endDate.getText().toString());
+					currentTask.set_quantity(Integer.parseInt(taskQuantity
+							.getText().toString()));
+					currentTask.set_description(taskDesc.getText().toString());
+
+					db.updateTask(currentTask, "");
+
+					finish();
+				}
+			}
+		});
+
+		initializeDates();
+	}
+
+	/**
+	 * Initializes the date fields, and sets up the DatePicker dialog.
+	 */
+	private void initializeDates() {
+		selectDate = (Button) findViewById(R.id.selectDate);
+		endDate = (TextView) findViewById(R.id.textEditDueDate);
+		final Calendar cal = Calendar.getInstance();
+
+		year = cal.get(Calendar.YEAR);
+		month = cal.get(Calendar.MONTH);
+		day = cal.get(Calendar.DAY_OF_MONTH);
+
+		cYear = year;
+		cMonth = month;
+		cDay = day;
+		
+		// Parses Date and turns into readable/setable format
+		String stringToParse = endDate.getText().toString();
+		SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd");
+		java.util.Date d1 = null;
+		try {
+			d1 = format.parse(stringToParse);
+		} catch (java.text.ParseException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		cal.setTime(d1);
+		year = cal.get(Calendar.YEAR);
+		month = cal.get(Calendar.MONTH);
+		day = cal.get(Calendar.DAY_OF_MONTH);
+
+		selectDate.setOnClickListener(new OnClickListener() {
+
+			@Override
+			public void onClick(View v) {
+				DatePickerDialog c = new DatePickerDialog(v.getContext(),
+						mDateSetListener, year, month, day);
+				c.show();
+			}
+
+		});
+	}
+
+	/**
+	 * This is the pop-up for when the button Select Date. It takes the data
+	 * from the user's input and copies it into year, month, and day.
+	 */
+	private DatePickerDialog.OnDateSetListener mDateSetListener = new DatePickerDialog.OnDateSetListener() {
+		public void onDateSet(DatePicker v, int inyear, int inmonth, int inday) {
+			year = inyear;
+			month = inmonth;
+			day = inday;
+
+			String dateDue = year + "-" + (month + 1) + "-" + day;
+			endDate.setText(dateDue);
+
+			Toast.makeText(v.getContext(),
+					"Date set to: " + endDate.getText().toString(),
+					Toast.LENGTH_SHORT).show();
+		}
+	};
+
+	/**
+	 * Checks for a valid input date.
+	 * 
+	 * @return Returns true if date is the current date or after. Returns false
+	 *         otherwise.
+	 */
+	private boolean checkDate() {
+		if (year < cYear)
+			return false;
+		if (year == cYear && month < cMonth)
+			return false;
+		if (year == cYear && cMonth == month && day < cDay)
+			return false;
+
+		return true;
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		getMenuInflater().inflate(R.menu.activity_update_task, menu);
+		return true;
+	}
+
+	// @Override
+	// public boolean onOptionsItemSelected(MenuItem item) {
+	// switch (item.getItemId()) {
+	// case android.R.id.home:
+	// NavUtils.navigateUpFromSameTask(this);
+	// return true;
+	// }
+	// return super.onOptionsItemSelected(item);
+	// }
 
 }
