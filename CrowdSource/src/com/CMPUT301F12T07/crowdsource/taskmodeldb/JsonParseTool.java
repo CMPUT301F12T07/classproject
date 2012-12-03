dce79d27a354fb9139e05247f8f5cfcc34503a7e
ekoedmedia <ekoedmedia@gmail.com>
2012-12-02 20:06:34 -0700

    - Adding missing files for last commit

---
+++ CrowdSource/src/com/CMPUT301F12T07/crowdsource/taskmodeldb/JsonParseTool.java
@@ -0,0 +1,118 @@
+package com.CMPUT301F12T07.crowdsource.taskmodeldb;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Random;
+
+import com.google.gson.JsonArray;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
+
+public class JsonParseTool {
+
+	// parse task list for HomeScreen
+	public static List<Task> parseTaskList(String jsonStringVersion) {
+		List<Task> taskList = new ArrayList<Task>();
+		// TODO: Convert to a Regular Expression to remove "{ }" and \" \":
+		jsonStringVersion = jsonStringVersion.replace("\"{", "{");
+		jsonStringVersion = jsonStringVersion.replace("}\"", "}");
+		jsonStringVersion = jsonStringVersion.replace("\\", "");
+		JsonElement jsonElement = new JsonParser().parse(jsonStringVersion);
+		JsonArray array = jsonElement.getAsJsonArray();
+
+		String wid;
+		String title;
+		String dateDue;
+		int quantity;
+		int qty_filled;
+		String type;
+
+		Iterator iterator = array.iterator();
+		while (iterator.hasNext()) {
+			JsonObject jsonObject = (JsonObject) iterator.next();
+
+			// get wid
+			JsonElement widElement = jsonObject.get("id");
+			wid = widElement.getAsString();
+
+			// get summary
+			JsonObject summaryObject = jsonObject.getAsJsonObject("summary");
+			title = summaryObject.get("_title").getAsString();
+			dateDue = summaryObject.get("_dateDue").getAsString();
+			quantity = summaryObject.get("_quantity").getAsInt();
+			qty_filled = summaryObject.get("_qty_filled").getAsInt();
+			type = summaryObject.get("_type").getAsString();
+
+			Task task = new Task(wid, title, dateDue, quantity, qty_filled,
+					type);
+			taskList.add(task);
+
+		}
+
+		return taskList;
+	}
+
+	/*
+	 * // Parse a single RemoteTask public static RemoteTask parseRemoteTask
+	 * (String jsonStringVersion) {
+	 * 
+	 * long tid; String uid; String title; String description; String
+	 * dateCreate; String dateDue; String type; int visibility; // 1 or 0 int
+	 * quantity; int qty_filled; int followed; // 1 or 0 int num_followed;
+	 * String user_email; String wid;
+	 * 
+	 * JsonObject jsonObject = (JsonObject) new
+	 * JsonParser().parse(jsonStringVersion);
+	 * 
+	 * JsonElement tidElement = jsonObject.get("tid"); tid =
+	 * tidElement.getAsLong();
+	 * 
+	 * JsonElement uidElement = jsonObject.get("uid"); uid =
+	 * uidElement.getAsString();
+	 * 
+	 * JsonElement titleElement = jsonObject.get("title"); title =
+	 * titleElement.getAsString();
+	 * 
+	 * JsonElement descriptionElement = jsonObject.get("description");
+	 * description = descriptionElement.getAsString();
+	 * 
+	 * JsonElement dateCreateElement = jsonObject.get("dateCreate"); dateCreate
+	 * = dateCreateElement.getAsString();
+	 * 
+	 * JsonElement dateDueElement = jsonObject.get("dateDue"); dateDue =
+	 * dateDueElement.getAsString();
+	 * 
+	 * JsonElement typeElement = jsonObject.get("type"); type =
+	 * typeElement.getAsString();
+	 * 
+	 * JsonElement visibilityElement = jsonObject.get("visibility"); visibility
+	 * = visibilityElement.getAsInt();
+	 * 
+	 * JsonElement quantityElement = jsonObject.get("quantity"); quantity =
+	 * quantityElement.getAsInt();
+	 * 
+	 * JsonElement qtyfilledElement = jsonObject.get("qty_filled"); qty_filled =
+	 * qtyfilledElement.getAsInt();
+	 * 
+	 * JsonElement followedElement = jsonObject.get("followed"); followed =
+	 * followedElement.getAsInt();
+	 * 
+	 * JsonElement numfollowedElement = jsonObject.get("num_followed");
+	 * num_followed = numfollowedElement.getAsInt();
+	 * 
+	 * JsonElement useremailElement = jsonObject.get("user_email"); user_email =
+	 * useremailElement.getAsString();
+	 * 
+	 * JsonElement widElement = jsonObject.get("wid"); wid =
+	 * widElement.getAsString();
+	 * 
+	 * // Create task object Task task = new Task(tid, uid, title, description,
+	 * dateCreate, dateDue, type, visibility, quantity, qty_filled, followed,
+	 * num_followed, user_email);
+	 * 
+	 * // Create RemoteTask object RemoteTask remoteTask = new RemoteTask(task,
+	 * wid); return remoteTask; }
+	 */
+}
---
+++ CrowdSource/src/com/CMPUT301F12T07/crowdsource/taskmodeldb/TaskSummary.java
@@ -0,0 +1,87 @@
+package com.CMPUT301F12T07.crowdsource.taskmodeldb;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+
+public class TaskSummary {
+
+	private String _wid;
+	private String _title;
+	private long _dateDue;
+	private int _quantity;
+	private int _qty_filled;
+	private String _type;
+	
+	/** constructor with wid */
+    public TaskSummary(String webid, String title, String dateDue, int quantity, int qty_filled, String type) { 
+    	this._wid = webid;
+    	this._title = title;
+    	set_dateDue(dateDue);
+    	this._quantity = quantity;
+    	this._qty_filled = qty_filled;
+    	this._type = type;
+    }
+    
+    public String get_title() {
+		return _title;
+	}
+
+	public void set_title(String _title) {
+		this._title = _title;
+	}
+	
+	public String get_dateDue() {
+		Date date = new Date(_dateDue);
+	    SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd");
+	    return format.format(date).toString();
+	}
+
+	public void set_dateDue(String _dateDue) {
+		Calendar cal = Calendar.getInstance();
+		SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd");
+		Date date = null;
+		try {
+			date = format.parse(_dateDue);
+		} catch (ParseException e) {
+			e.printStackTrace();
+		}
+		cal.setTime(date);
+		this._dateDue = cal.getTime().getTime();
+	}
+	
+	public String get_type() {
+		return _type;
+	}
+
+	public void set_type(String _type) {
+		this._type = _type;
+	}
+	
+	public int get_quantity() {
+		return _quantity;
+	}
+
+	public void set_quantity(int _quantity) {
+		this._quantity = _quantity;
+	}
+	
+	public int get_qty_filled() {
+		return _qty_filled;
+	}
+	
+	public void set_qty_filled(int _qty_filled) {
+		this._qty_filled = _qty_filled;
+	}
+	
+	public String get_wid() {
+		return _wid;
+	}
+	
+	public void set_wid(String webid) {
+		this._wid = webid;
+	}
+	
+	
+}
